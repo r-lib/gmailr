486acb0d901453d16ff67920b23833f493e77af6
Jim Hester <james.f.hester@gmail.com>
2019-08-13 08:55:13 -0400

    Correctly skip tests if not authenticated

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,12 +1,21 @@
 skip_unless_authenticated <- function() {
-  if (!is.null(tryCatch(gm_oauth_app(), error = function(e) NULL))) {
+  maybe_app <- function() {
+    tryCatch(gm_oauth_app(), error = function(e) NULL)
+  }
+
+  if (!is.null(maybe_app())) {
     return()
   }
 
-  gm_auth_configure()
-  gm_auth(cache = TRUE, use_oob = FALSE)
+  tryCatch(
+    expr = {
+      gm_auth_configure()
+      gm_auth(cache = TRUE, use_oob = FALSE)
+    },
+    error = function(e) e
+  )
 
-  if (is.null(gm_oauth_app())) {
+  if (is.null(maybe_app())) {
     skip("not authenticated")
   }
 }
