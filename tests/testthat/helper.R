ff637b6634f229759c9efeb6cff128b9995524ca
Jim Hester <james.f.hester@gmail.com>
2019-08-16 14:18:18 -0400

    Simplify script

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,15 +1,10 @@
 str(Sys.getenv("GMAILR_EMAIL"))
-options(gargle_quiet = FALSE)
 if (gargle:::secret_can_decrypt("gmailr")) {
   # we want to use the oauth token directly, this avoids the need to reproduce the token filename
   token <- unserialize(gzcon(rawConnection(gargle:::secret_read("gmailr", "rpkgtester@gmail.com"))))
   dir.create(token$cache_path)
   gm_auth_configure()
-  gargle::credentials_byo_oauth2(token = token)
-  cred <- gargle::credentials_byo_oauth2(token = token)
-  .auth$set_cred(cred)
-  .auth$set_auth_active(TRUE)
-  #gm_auth(token = token)
+  gm_auth(token = token)
 }
 
 skip_if_no_token <- function() {
