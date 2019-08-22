8fd77010a9a1e70985ff88f21e22c90e76e27c1f
Jim Hester <james.f.hester@gmail.com>
2019-08-22 10:37:55 -0400

    Convert calls to create, modify, delete label

--- tests/testthat/test-label.R
+++ tests/testthat/test-label.R
@@ -13,7 +13,7 @@ test_that("labels work", {
 test_that("label works", {
   skip_if_no_token()
 
-  my_label <- label("SENT")
+  my_label <- gm_label("SENT")
 
   expect_equal(my_label$id, "SENT")
   expect_equal(my_label$type, "system")
@@ -24,21 +24,21 @@ test_that("create_label, update_label, update_label_patch and delete_label work"
   skip_if_no_token()
 
   label_name1 <- basename(tempfile(pattern = "foo"))
-  new_label <- create_label(label_name1)
+  new_label <- gm_create_label(label_name1)
   expect_equal(new_label$name, label_name1)
 
   label_name2 <- basename(tempfile(pattern = "bar"))
   new_label$name <- label_name2
 
-  new_label2 <- update_label(new_label$id, new_label)
+  new_label2 <- gm_update_label(new_label$id, new_label)
   expect_equal(new_label2$name, label_name2)
 
   label_name3 <- basename(tempfile(pattern = "baz"))
   new_label2$name <- label_name3
-  new_label3 <- update_label_patch(new_label2$id, new_label2)
+  new_label3 <- gm_update_label_patch(new_label2$id, new_label2)
   expect_equal(new_label3$name, label_name3)
 
-  delete_label(new_label3$id)
+  gm_delete_label(new_label3$id)
 
-  expect_error(label(new_label3$id))
+  expect_error(gm_label(new_label3$id))
 })
