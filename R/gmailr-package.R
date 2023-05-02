323096faa6eacdf19e63a021b4fe22707dedb86a
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-05-02 13:38:51 -0700

    use_tidy_dependencies()

--- R/gmailr-package.R
+++ R/gmailr-package.R
@@ -5,6 +5,7 @@
 #' @import base64enc
 #' @import httr
 #' @import rlang
+#' @importFrom glue glue
 #' @importFrom lifecycle deprecate_soft
 #' @importFrom lifecycle deprecate_warn
 #' @importFrom lifecycle deprecated
