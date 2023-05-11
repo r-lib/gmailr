4802d8601e13e954af472726bd306790c712918b
Jenny Bryan <jenny.f.bryan@gmail.com>
2023-05-11 15:11:24 -0700

    Configure a dummy oauth client

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -5,8 +5,16 @@ if (gargle:::secret_can_decrypt("gmailr")) {
   )))
   dir.create(token$cache_path, showWarnings = FALSE)
 
-  # Is this really necessary? I doubt it
-  gm_auth_configure(client = token$app)
+  # https://github.com/r-lib/gmailr/issues/160
+  fake_client <- gargle::gargle_oauth_client(
+    id = "PLACEHOLDER",
+    secret = "PLACEHOLDER"
+  )
+  gm_auth_configure(fake_client)
+  # Alternatively, I could do this:
+  # gm_auth_configure(client = token$app)
+  # But that is somewhat misleading, i.e. it suggests the client needs to match
+  # that of the token, which it does not.
 
   gm_auth(token = token)
 
